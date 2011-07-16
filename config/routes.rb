060f89a2136e919b976d45fe614e5129b5441440
Tomasz Bak <t.bak@selleo.com>
2011-07-16 16:30:55 +0200

    new sort

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,12 @@ Kb::Engine.routes.draw do
   resources :sections do
     resources :articles
   end
-
   root :to => 'sections#index'
 
   namespace :admin do
     resources :sections do
       resources :articles
     end
+    resource :sections_sort, :controller => 'sections/sort'
   end
-
 end
